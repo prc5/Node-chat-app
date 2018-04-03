07b1268fbac0be2d5806ccac94dd23235604ad38
prc5 <m.pyrc@interia.pl>
2018-04-03 16:37:44 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,7 +14,17 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log("new user connected");
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat'
+    })    
     
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined'
+    })
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message)
         io.emit('newMessage', {
@@ -22,8 +32,13 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         })
+        //        socket.$broadcast.$emit('newMessage', {
+        //            from: message.from,
+        //            text: message.text,
+        //            createdAt: new Date().getTime()
+        //        })
     });
-    
+
     socket.on('disconnect', () => {
         console.log('user was dissconnected')
     })
@@ -33,4 +48,6 @@ server.listen(port, () => {
     console.log('Started up on port', port);
 });
 
-module.exports = {app};
+module.exports = {
+    app
+};
