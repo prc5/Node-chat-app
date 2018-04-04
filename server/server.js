cc0d96d75442b92863a53726b731102e0a5388b7
prc5 <m.pyrc@interia.pl>
2018-04-04 19:00:06 +0200

    Added users class and updateList

--- server/server.js
+++ server/server.js
@@ -7,20 +7,35 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 
 let app = express();
 let server = http.createServer(app);
 let io = socketIO(server);
+let users = new Users();
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log("new user connected");
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the app'));
+    socket.on('join', (params, calback) => {
+        if(!isRealString(params.name) || !isRealString(params.room)) {
+            return calback('Name and room name are required.')
+        }
+        
+        socket.join(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
+        
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the app'));
+        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
+        
+        calback();
+    });
     
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
-
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage',generateMessage(message.from, message.text));
@@ -32,7 +47,11 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log('user was dissconnected')
+        let user = users.removeUser(socket.id);
+        if (user) {
+           io.to(user.room).emit('updateUserList', users.getUserList(user.room)); 
+           io.to(user.room).emit('newMessage', generateMessage('Admin',`${user.name} has left`)); 
+        }
     })
 });
 
