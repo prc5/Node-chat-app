50d16496c75e3e13dd647356815df148e77963b0
prc5 <m.pyrc@interia.pl>
2018-04-04 17:44:00 +0200

    Added auto scroll function

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+    //Selectors
+    let messages = jQuery('#messages');
+    let newMessage = messages.children('li:last-child')
+    //Heights
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+    
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight>= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('connected to server');
 });
@@ -18,6 +34,7 @@ socket.on('newMessage', function (message) {
     });
     
     jQuery("#messages").append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -31,6 +48,7 @@ matedTime = moment(message.createdAt).format('h:mm a');
     });
     
     jQuery("#messages").append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e) {
